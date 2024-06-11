6c4a24e6693217a26d9f0d26f6f743ce67e510f1
TechMadi <winnieamandela@gmail.com>
2024-06-11 03:39:19 +0300

    Update: Adding Tags

--- tests/booking.get.spec.ts
+++ tests/booking.get.spec.ts
@@ -1,7 +1,13 @@
 import { test, expect, request } from "@playwright/test";
 
-test.describe("GET  All booking", async () => {
-  test("Open url", async ({ page }) => {
-    await page.goto(`${process.env.BASE_URL}`);
-  });
-});
+test.describe(
+  "GET  All booking",
+  {
+    tag: "@api",
+  },
+  async () => {
+    test("Open url", async ({ page }) => {
+      await page.goto(`${process.env.BASE_URL}`);
+    });
+  }
+);
--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -1,13 +1,19 @@
 import test, { expect } from "@playwright/test";
 
-test.describe("Open Local Host", async () => {
-  test("Open Local host ", async ({ page }) => {
-    await page.goto(process.env.CLIENT_BASE_URL as string);
+test.describe(
+  "Open Local Host",
+  {
+    tag: "@e2e",
+  },
+  async () => {
+    test("Open Local host ", async ({ page }) => {
+      await page.goto(process.env.CLIENT_BASE_URL as string);
 
-    await expect(page).toHaveTitle("Create Next App");
-  });
+      await expect(page).toHaveTitle("Create Next App");
+    });
 
-  test("Learn Next Js", async ({ page }) => {
-    // await page.goto(process.env.CLIENT_BASE_URL as string) ;
-  });
-});
+    test("Learn Next Js", async ({ page }) => {
+      // await page.goto(process.env.CLIENT_BASE_URL as string) ;
+    });
+  }
+);
