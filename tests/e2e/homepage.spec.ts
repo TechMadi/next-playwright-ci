51a5219de340a0d78e2f13528d54cf4c59c904fc
TechMadi <winnieamandela@gmail.com>
2024-06-13 10:06:14 +0300

    fix: using set up file

--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -1,11 +1,15 @@
 import test, { expect } from "@playwright/test";
+import { userAuthFile } from "../setup/auth-user.setup";
 
 test.describe(
   "Open Local Host",
   {
     tag: "@e2e",
   },
+
   async () => {
+    test.use({ storageState: userAuthFile });
+
     test("Open Local host ", async ({ page }) => {
       await page.goto(process.env.CLIENT_BASE_URL as string);
 
