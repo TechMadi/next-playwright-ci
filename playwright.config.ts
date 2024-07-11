b2e522f958eb94c9ba22ac2e70acf406f366d573
TechMadi <winnieamandela@gmail.com>
2024-07-11 17:54:07 +0300

    changing directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ dotenv.config({ path: path.resolve(__dirname, "./", ".env.local") });
  */
 export default defineConfig({
   testDir: "./tests",
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html", { outputFolder: "blob-report" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
